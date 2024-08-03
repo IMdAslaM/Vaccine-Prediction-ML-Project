a1c35938545e241c86d0f451b01a6f986773d8c3
Mohamed Aslam I <121491315+IMdAslaM@users.noreply.github.com>
2024-08-04 00:04:42 +0530

    Update vaccine_pred_Final.py

--- vaccine_pred_Final.py
+++ vaccine_pred_Final.py
@@ -69,7 +69,7 @@ new_df['census_msa']=new_df['census_msa'].map(census_mapping)
 def page_home():
     st.title("Welcome to My Streamlit Dashboard")
 
-    st.markdown("""## <span style="color:red">Problem Statement of this ML application:</span>""",unsafe_allow_html=True)
+    st.markdown("""## <span style="color:aqua">Problem Statement of this ML application:</span>""",unsafe_allow_html=True)
     st.markdown('This project aims to predict the likelihood of people taking an H1N1 flu vaccine using Logistic Regression. It involves analyzing a dataset containing various features related to individuals\' behaviors, perceptions, and demographics, and building a predictive model to determine vaccine acceptance.')
     st.markdown('Predict the probability of individuals taking an H1N1 flu vaccine based on theircharacteristics and attitudes. This can help healthcare professionals and policymakers target vaccination campaigns more effectively.')
 
